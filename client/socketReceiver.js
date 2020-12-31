d0704bc402b4a1bdc1af2a392e71b17f4e36efd6
soupy <simonw.game@gmail.com>
2020-12-30 22:50:19 -0600

    get rid of js stuff

--- client/socketReceiver.js
+++
@@ -1,7 +0,0 @@
-socket.on("messageReceived", function (newMessage) {
-    var messages = document.getElementById("messages");
-    let div = document.createElement("div");
-    div.append(newMessage);
-    messages.append(div);
-});
-//# sourceMappingURL=socketReceiver.js.map
--- client/socketSender.js
+++
@@ -1,8 +0,0 @@
-var server;
-(function (server) {
-    function sendMessage(message) {
-        socket.emit("sendMessage", message);
-    }
-    server.sendMessage = sendMessage;
-})(server || (server = {}));
-//# sourceMappingURL=socketSender.js.map
