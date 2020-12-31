840ffc7090ad86f9d6b304203c542687a680c1f8
soupy <simonw.game@gmail.com>
2020-12-31 00:33:42 -0600

    same

--- app.ts
+++ app.ts
@@ -6,7 +6,7 @@ const app = express();
 //const http = require('http');
 
 
-const port = process.env.port || 8080;
+const port = process.env.port || 3000;
 
 
 //http.createServer(function (req, res) {
